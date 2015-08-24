b7721f9a570a42bb68546fffd437efe837833e28
Emerson Loustau <e@thesecretaddress.com>
2015-08-23 22:33:04 -0400

    prefixes and namespaces

--- tests/test-github.php
+++ tests/test-github.php
@@ -1,12 +1,12 @@
 <?php
 
-class GithubTest extends WP_UnitTestCase {
+class WPGHPL_GithubTest extends WP_UnitTestCase {
 
 	function setUp() {
 		
 		parent::setUp();
 
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		
 		# mandatory config settings to prevent message
 		update_option($gh->prefix . 'gh_repo', 'jquery');
@@ -17,13 +17,13 @@ class GithubTest extends WP_UnitTestCase {
 
 	function test_defaults() {
 
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		#defaults to page 1
 		$this->assertEquals( 1, $gh->page );
 
 		#reacts to get params for pages
 		$_GET['gh_page'] = 6;
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		$this->assertEquals( 6, $gh->page );
 
 		$this->assertTrue( isset($gh->has_settings) );
@@ -32,17 +32,17 @@ class GithubTest extends WP_UnitTestCase {
 	function test_check_for_settings() {
 
 		# returns true for repo + org when repo is public
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		$this->assertTrue( $gh->has_settings );
 
 		# returns false for repo + org when repo is private (needs token)
 		update_option($gh->prefix . 'repo_is_public', 0);
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		$this->assertFalse( $gh->has_settings );
 
 		# returns true for repo + org when repo is private and token is set
 		update_option($gh->prefix . 'token', 'abcxyz');
-		$gh = new Github();
+		$gh = new WPGHPL_Github();
 		$this->assertTrue( $gh->has_settings );
 
 	}
